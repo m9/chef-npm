d2f5f26e22053c0accb2e2f04d76f01144bcc136
Mathias Buus <mathiasbuus@gmail.com>
2014-02-17 21:33:23 +0100

    Fixed versioned package existance check

--- providers/package.rb
+++ providers/package.rb
@@ -5,7 +5,7 @@ action :install do
   pkg_id += "@#{new_resource.version}" if new_resource.version
   execute "install NPM package #{new_resource.name}" do
     command "npm -g install #{pkg_id}"
-    not_if "npm -g ls '#{pkg_id}' 2> /dev/null | grep ' #{pkg_id}@'"
+    not_if "npm -g ls '#{pkg_id}' 2> /dev/null | grep ' #{pkg_id}'"
   end
 end
 
@@ -16,7 +16,7 @@ action :install_local do
   execute "install NPM package #{new_resource.name} into #{path}" do
     cwd path
     command "npm install #{pkg_id}"
-    not_if "cd #{path} && npm ls '#{pkg_id}' 2> /dev/null | grep ' #{pkg_id}@'"
+    not_if "cd #{path} && npm ls '#{pkg_id}' 2> /dev/null | grep ' #{pkg_id}'"
   end
 end
 
@@ -34,7 +34,7 @@ action :uninstall do
   pkg_id += "@#{new_resource.version}" if new_resource.version
   execute "uninstall NPM package #{new_resource.name}" do
     command "npm -g uninstall #{pkg_id}"
-    only_if "npm -g ls '#{pkg_id}' 2> /dev/null | grep ' #{pkg_id}@'"
+    only_if "npm -g ls '#{pkg_id}' 2> /dev/null | grep ' #{pkg_id}'"
   end
 end
 
@@ -45,6 +45,6 @@ action :uninstall_local do
   execute "uninstall NPM package #{new_resource.name} from #{path}" do
     cwd path
     command "npm uninstall #{pkg_id}"
-    only_if "cd #{path} && npm ls '#{pkg_id}' 2> /dev/null | grep ' #{pkg_id}@'"
+    only_if "cd #{path} && npm ls '#{pkg_id}' 2> /dev/null | grep ' #{pkg_id}'"
   end
 end
